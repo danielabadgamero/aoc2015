aa7f305c20eabf53e10c13b8a6332e63992ddc7c
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-01 17:46:37 +0200

    Finish day 2.

--- Day02/main.cpp
+++ Day02/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 int main()
 {
@@ -25,13 +26,40 @@ int main()
 				buf[selBuf].push_back(c);
 			}
 		dimensions[selBuf] = std::stoi(buf[selBuf]);
-		totalSurface +=
-			2 * dimensions[0] * dimensions[1] +
-			2 * dimensions[1] * dimensions[2] +
-			2 * dimensions[2] * dimensions[0];
+		int sideA{ 2 * dimensions[0] * dimensions[1] };
+		int sideB{ 2 * dimensions[1] * dimensions[2] };
+		int sideC{ 2 * dimensions[2] * dimensions[0] };
+		totalSurface += sideA + sideB + sideC + std::min(sideA, std::min(sideB, sideC)) / 2;
 	}
 
 	std::cout << "Part 1: " << totalSurface << "\n\n";
+
+	input.close();
+	input.open("input.txt");
+	int totalLength{};
+	while (input >> line)
+	{
+		int selBuf{};
+		std::string buf[3]{};
+		int dimensions[3]{};
+		for (const char& c : line)
+			switch (c)
+			{
+			case 'x':
+				dimensions[selBuf] = std::stoi(buf[selBuf]);
+				buf[selBuf].clear();
+				selBuf++;
+				break;
+			default:
+				buf[selBuf].push_back(c);
+			}
+		dimensions[selBuf] = std::stoi(buf[selBuf]);
+		
+		std::sort(dimensions, dimensions + 3);
+		totalLength += 2 * dimensions[0] + 2 * dimensions[1] + dimensions[0] * dimensions[1] * dimensions[2];
+	}
+
+	std::cout << "Part 2: " << totalLength << "\n\n";
 
 	return 0;
 }
