1620f70ed987beb021a4d363b3b9b356a37a3352
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-01 17:15:32 +0200

    Complete day 1 and add day 2.

--- Day01/main.cpp
+++ Day01/main.cpp
@@ -4,6 +4,22 @@
 int main()
 {
 	std::ifstream input{ "input.txt" };
+	int floor{};
+	bool found{};
+	int pos{};
+	while (!input.eof())
+	{
+		if (!found)
+			pos++;
+		char c{};
+		input.read(&c, 1);
+		if (c == '(') floor++;
+		if (c == ')') floor--;
+		if (floor == -1) found = true;
+	}
+
+	std::cout << "Part 1: " << floor << "\n\n";
+	std::cout << "Part 1: " << pos << "\n\n";
 
 	return 0;
 }
