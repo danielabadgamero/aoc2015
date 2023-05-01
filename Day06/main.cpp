c51ada02a1baae45b9c680e04a0f9ccee42160a1
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-01 19:27:04 +0200

    Add day 6.

--- Day06/main.cpp
+++ Day06/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 #include <fstream>
 
+struct Light
+{
+	bool status{};
+
+	void toggle() { status = !status; }
+	void on() { status = true; }
+	void off() { status = false; }
+};
+
+Light lights[1000][1000]{};
+
 int main()
 {
 	std::ifstream input{ "input.txt" };
+	std::string instruction{};
+	while (input >> instruction)
+		if (instruction.find("on"))
+		{
+
+		}
+		else if (instruction.find("off"))
+		{
+
+		}
+		else
+		{
+
+		}
 
 	return 0;
 }
