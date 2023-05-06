0d89de1609ceb594319ccf5b2b080e53df5d8940
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-05-06 17:41:32 +0200

    Finish day 12.

--- Day12/main.cpp
+++ Day12/main.cpp
@@ -3,9 +3,49 @@
 #include <string>
 #include <vector>
 
-static bool insideArray(size_t pos, std::string& obj)
+std::vector<std::string> getElements(const std::string& parent)
 {
+	std::vector<std::string> elements(1);
+	int nestLevel{};
+	for (size_t i{}; i != parent.size(); i++)
+	{
+		switch (parent[i])
+		{
+		case '[': case '{': nestLevel++; break;
+		case ']': case '}': nestLevel--; break;
+		}
+		if (parent[i] == ',' && nestLevel == 1)
+			elements.push_back("");
+		else if (i != 0 && i + 1 != parent.size())
+			elements.back().push_back(parent[i]);
+	}
 
+	return elements;
+}
+
+int add(const std::string& obj)
+{
+	int sum{};
+	std::vector<std::string> elements{ getElements(obj) };
+	for (const std::string& elem : elements)
+	{
+		if (elem[0] != '{' && elem[0] != '[')
+		{
+			if (elem.back() == ']' || elem.back() == '}')
+				sum += add(elem.substr(elem.find(':') + 1));
+			else if (elem.find(":\"red\"") != std::string::npos)
+				return 0;
+			try
+			{
+				sum += std::stoi(elem.substr(elem.find(':') + 1));
+			}
+			catch (...) {}
+		}
+		else
+			sum += add(elem);
+	}
+
+	return sum;
 }
 
 int main()
@@ -31,16 +71,11 @@ int main()
 	input.open("input.txt");
 	std::string line{};
 	input >> line;
-
 	size_t pos{};
-	while (pos != std::string::npos)
-	{
-		int nestLevel{};
-		pos = line.find("\"red\"", pos + 1);
-		if (insideArray(pos, line))
-	}
+	pos = line.find("\"red\"", pos + 1);
+	numBuf.clear();
 
-	std::cout << "Part 2: " << sum << "\n\n";
+	std::cout << "Part 2: " << add(line) << "\n\n";
 
 	return 0;
 }
